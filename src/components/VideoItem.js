aff0b80e92e503d4d4a79d9cdb4ce5dab4136035
sergijacastmtfactory <sergi.jacas@tmtfactory.com>
2017-10-24 19:45:38 +0200

    Protoype with flow

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,20 +1,23 @@
+// @flow
+
 import React from 'react';
 import './VideoItem.css';
-import PropTypes from 'prop-types'
 
-const VideoItem = function(props) {
+type Video = {
+  image: string,
+  title: string
+};
+
+type Props = {
+  video: Video
+};
+
+const VideoItem = function(props: Props) {
   console.log(props);
     return (<div className="video-item">
     <h1>{props.video.title}</h1>
     <img src={props.video.image} alt={props.video.title} />
     </div>);
-}
-
-VideoItem.propTypes = {
-  video: PropTypes.shape({
-    image: PropTypes.string,
-    title: PropTypes.string
-  })
 }
 
 export default VideoItem;
